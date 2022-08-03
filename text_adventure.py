9d0bbe14f01a2a7d6b12f7ec5e4638681a9a8cd1
Paul Young <paulandresyoung@gmail.com>
2022-08-02 20:19:47 -0700

    clean up formatting

--- text_adventure.py
+++ text_adventure.py
@@ -1,5 +1,6 @@
 import random
 from collections import namedtuple
+from enum import Enum
 
 class Directions(object):
     NORTH = "north"
@@ -7,7 +8,10 @@ class Directions(object):
     EAST = "east"
     WEST = "west"
 
-Attack = namedtuple("Attack", [""])
+context = Enum("Context",
+    "navigate",
+    "fight"
+)
 
 class Enemy(object):
     def __init__(self, hp=100, attacks:list=None):
@@ -49,72 +53,45 @@ def navigate(room:Room):
 
     print(f"You are in {room.name}")
     if room.enemies:
-
         combat(enemies)
     
     if room.items:
-
         print("You see:")
         for item in room.items:
-
             print(f"  -{item.name}")
 
-
     print("You may go:")
-
     for direction in room.connections.keys():
-
         print(direction)
-    
 
     return input("What would you like to do? (enter 'o' for options)").strip()
     
 
 def play_game(starting_room:Room):
-
     current_room = starting_room
-
-
     while True:
-
         user_input = navigate(current_room)
-
-
         if user_input in current_room.connections:
-
             current_room = current_room.connections[user_input]
         else:
-
             print("Invalid entry")
 
-
 def combat(enemies):
-
     pass
 
 def main():
-
     entry = Room("Entryway")
-
     dungeon = Room("Dungeon")
-
     cellar = Room("Cellar")
-
     dining_room = Room("Dining Room")
-
     tower = Room("Tower")
 
-
     entry.add_connection(dining_room, Directions.EAST, Directions.WEST)
-
     dining_room.add_connection(cellar, Directions.SOUTH, Directions.NORTH)
-
     dining_room.add_connection(tower, Directions.EAST, Directions.WEST)
-
     cellar.add_connection(dungeon, Directions.SOUTH, Directions.NORTH)
 
     play_game(entry)
-
 
 if __name__ == "__main__":
     main()
